6214bb367b8e80986002a751d572df771fd964bd
hadoop <hadoop@navy>
2015-02-03 16:29:04 +0800

    adsf

---
+++ com.hadoop.runtime/src/main/java/com/hadoop/runtime/Main.java
@@ -0,0 +1,11 @@
+package com.hadoop.runtime;
+
+import com.hadoop.trial.mapreduce.MapReduceTest;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		MapReduceTest.main(null);
+	}
+}
