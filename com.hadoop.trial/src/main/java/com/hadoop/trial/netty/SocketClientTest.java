01238a55c541affac71caf904c66695d4765714e
hadoop <hadoop@navy>
2015-02-11 08:59:45 +0800

    asdf

---
+++ com.hadoop.trial/src/main/java/com/hadoop/trial/netty/SocketClientTest.java
@@ -0,0 +1,9 @@
+package com.hadoop.trial.netty;
+
+public class SocketClientTest
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
---
+++ com.hadoop.trial/src/main/java/com/hadoop/trial/netty/SocketServerTest.java
@@ -0,0 +1,9 @@
+package com.hadoop.trial.netty;
+
+public class SocketServerTest
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
